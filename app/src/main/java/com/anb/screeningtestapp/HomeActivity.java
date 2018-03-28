11bf9603756fac40f685b0220c026877bbdbb074
Agung Nursatria <agungnursatria@gmail.com>
2018-03-29 06:58:01 +0700

    Adding palindrom, prime month checking, adding some picture

--- app/src/main/java/com/anb/screeningtestapp/HomeActivity.java
+++ app/src/main/java/com/anb/screeningtestapp/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.anb.screeningtestapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,12 +24,49 @@ public class HomeActivity extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!edtNama.getText().toString().equals("")){
-                    Intent intent = new Intent(HomeActivity.this,SelectingActivity.class);
-                    intent.putExtra("nama", edtNama.getText().toString());
-                    startActivity(intent);
+                final String nama = edtNama.getText().toString();
+                if (nama != ""){
+                    boolean palindromCheck = isPalindrom(nama);
+                    String palindromWord = palindromCheck ? "isPalindrom" : "not palindrome";
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                    builder.setTitle(String.valueOf(palindromCheck).toUpperCase())
+                            .setMessage(palindromWord)
+                            .setCancelable(false)
+                            .setPositiveButton("Next", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                    Intent intent = new Intent(HomeActivity.this, SelectingActivity.class);
+                                    intent.putExtra("nama", nama);
+                                    startActivity(intent);
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                    Button buttonPositive = alert.getButton(DialogInterface.BUTTON_POSITIVE);
+                    buttonPositive.setTextColor(Color.parseColor("#000000"));
                 }
             }
         });
+    }
+
+    private boolean isPalindrom(String sentence){
+        sentence = sentence.replaceAll("\\s", "");
+        Log.d("Palindrom Check", sentence);
+        int top = sentence.length()-1;
+        int bottom = 0;
+
+        while (top >= bottom && sentence.charAt(bottom)==sentence.charAt(top)){
+            Log.d("Palindrom Check", "Senternce[top]: " + sentence.charAt(top) + "\n"
+                    + "Senternce[bottom]: " + sentence.charAt(bottom) + "\n"
+                    + "top: " + top + "\n"
+                    + "bottom: " + bottom + "\n"
+                    + "==================================================== \n \n"
+            );
+            top--;
+            bottom++;
+        }
+        return (top < bottom);
     }
 }
--- app/src/main/java/com/anb/screeningtestapp/SelectingActivity.java
+++ app/src/main/java/com/anb/screeningtestapp/SelectingActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -60,35 +61,56 @@ public class SelectingActivity extends AppCompatActivity {
             if (resultCode == Activity.RESULT_OK){
                 btnPilihGuest.setText(data.getStringExtra("namaguest"));
 
-                try {
-                    String dateString = data.getStringExtra("birthdayguest");
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                    Date date = sdf.parse(dateString);
-                    int startDate = (int) (date.getTime()/1000);
+                String dateString = data.getStringExtra("birthdayguest");
+                jenisOS(dateString);
 
-                    String os;
-                    if (startDate % 2 == 0  && startDate % 3 == 0 ){
-                        os = "iOS";
-                    }
-                    else if (startDate % 3 == 0){
-                        os = "android";
-                    }
-                    else if (startDate % 2 == 0){
-                        os = "blackberry";
-                    }
-                    else
-                    {
-                        os = "feature phone";
-                    }
-
-                    Toast.makeText(this, os, Toast.LENGTH_LONG).show();
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
+                isMonthPrime(dateString);
             }
         }
+    }
 
+    public void jenisOS(String dateString){
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = sdf.parse(dateString);
+            int startDate = (int) (date.getTime()/1000);
 
+            String os;
+            if (startDate % 2 == 0  && startDate % 3 == 0 ){
+                os = "iOS";
+            }
+            else if (startDate % 3 == 0){
+                os = "android";
+            }
+            else if (startDate % 2 == 0){
+                os = "blackberry";
+            }
+            else
+            {
+                os = "feature phone";
+            }
+
+            Toast.makeText(this, os, Toast.LENGTH_SHORT).show();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void isMonthPrime(String dateString){
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = sdf.parse(dateString);
+            int month = Integer.valueOf((String) DateFormat.format("MM",date));
+
+            int counter = 0;
+            for (int i=2;i<=month;i++){
+                if (month % i == 0)
+                    counter++;
+            }
+            String prime = (counter == 1)? "Bulan "+ month +" merupakan bilangan prima" : "Bulan "+ month +" bukan bilangan prima";
+            Toast.makeText(this, prime, Toast.LENGTH_SHORT).show();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
